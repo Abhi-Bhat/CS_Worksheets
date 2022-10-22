44cdfd84b8c2d349cbb0d50fc63bc2f07c061d2b
Abhi-Bhat <104887580+Abhi-Bhat@users.noreply.github.com>
2022-10-22 11:27:08 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,44 @@
+'''
+DAY-1 WORKSHEET SOLUTIONS-Abhyuday 
+
+A1)
+
+1   2   
+
+1   2   3
+
+1   2   3   4
+
+A2)
+I
+N
+D
+I
+A
+
+A3)
+1 I
+2 N
+3 D
+4 I
+5 A
+
+
+A4)
+Current variable value: 7
+Current variable value: 6
+Current variable value: 5
+Current variable value: 4  
+aaa
+bbb
+ccc
+Good Bye
+
+A5)
+Statement 4
+
+'''
+
+
+
+
